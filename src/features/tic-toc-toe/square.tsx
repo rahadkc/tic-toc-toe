5e82a4d5ce6a7bfcd1fd49a97a8f64c58b7410db
rahadkc <rahadshz@gmail.com>
2021-10-03 16:13:02 +0600

    Square JSX.Element value type added

--- src/features/tic-toc-toe/square.tsx
+++ src/features/tic-toc-toe/square.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 export type ISquareProps = {
-  value: string | null
+  value: string | null | JSX.Element
   onClick: () => void
 }
 
