296583417554b160df26f6cf8b6f951cc88ee6c6
rahadkc <rahadshz@gmail.com>
2021-10-04 23:45:04 +0600

    Text app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
 import { render } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(
+test('render App component without crash', () => {
+  const element = render(
     <Provider store={store}>
       <App />
     </Provider>
   )
-
-  expect(getByText(/hello/i)).toBeInTheDocument()
+  expect(element).toMatchSnapshot()
 })
