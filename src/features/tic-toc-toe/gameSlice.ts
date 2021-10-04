da811b06027933552e0a490049d85a1cecd06f52
rahadkc <rahadshz@gmail.com>
2021-10-04 09:59:17 +0600

    Add gameStatus action to store game status in redux

--- src/features/tic-toc-toe/gameSlice.ts
+++ src/features/tic-toc-toe/gameSlice.ts
@@ -1,12 +1,14 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '../../app/store'
 
 export interface InitialState {
   reset: boolean
+  status: string
 }
 
 const initialState: InitialState = {
-  reset: false
+  reset: false,
+  status: ''
 }
 
 export const gameSlice = createSlice({
@@ -15,12 +17,16 @@ export const gameSlice = createSlice({
   reducers: {
     resetGame: (state: InitialState) => {
       state.reset = !state.reset
+    },
+    gameStatus: (state: InitialState, action: PayloadAction<string>) => {
+      state.status = action.payload
     }
   }
 })
 
-export const { resetGame } = gameSlice.actions
+export const { resetGame, gameStatus } = gameSlice.actions
 
 export const selectReset = (state: RootState) => state.game.reset
+export const selectStatus = (state: RootState) => state.game.status
 
 export default gameSlice.reducer
