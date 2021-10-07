58d42e595d371ae2e96de83afcadb0bd1e27eea2
rahadkc <rahadshz@gmail.com>
2021-10-07 15:23:04 +0600

    Status: update soit will return with different kind of messages based of different kind of result

--- src/features/tic-toc-toe/status.tsx
+++ src/features/tic-toc-toe/status.tsx
@@ -1,20 +1,65 @@
 import React from 'react'
 import { useAppSelector } from '../../app/hooks'
 import { selectStatus } from './gameSlice'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 export type IStatusProps = {}
 
 const Status: React.FC<IStatusProps> = () => {
-  const status = useAppSelector(selectStatus)
-  return <GameStatus>{status}</GameStatus>
+  const { draw, win, next, winner } = useAppSelector(selectStatus)
+
+  const winnerContent = win ? (
+    <GameWin>
+      Hurrayyy!: <Trophy>&#x1F3C6;</Trophy> "{winner?.winner}" won the game.
+    </GameWin>
+  ) : (
+    <></>
+  )
+  const nextPlayerContent = !win && !draw ? <GameOn>Next player: {next}</GameOn> : <></>
+  const drawContent = draw ? <GameDraw>Game is draw!</GameDraw> : <></>
+
+  return (
+    <GameStatus>
+      {winnerContent}
+      {nextPlayerContent}
+      {drawContent}
+    </GameStatus>
+  )
 }
 
-const GameStatus = styled.p`
+const GameStatus = styled.div`
   color: var(--theme-color);
   font-size: 1.475rem;
   font-weight: bold;
   margin-top: 3rem;
   letter-spacing: 1px;
+`
+const Rotate = keyframes`
+  0% {
+    transform: rotateY(0deg)
+  }
+
+  40%, 60% {
+    transform: rotateY(180deg)
+  }
+
+  100% {
+    transform: rotateY(360deg)
+  }
+`
+
+const GameWin = styled.p`
+  color: var(--success-light);
+`
+const GameOn = styled.p``
+const GameDraw = styled.p`
+  color: var(--light);
+`
+
+const Trophy = styled.span`
+  display: inline-block;
+  width: 40px;
+  font-size: 40px;
+  animation: ${Rotate} 2s linear forwards infinite;
 `
 export default React.memo(Status)
