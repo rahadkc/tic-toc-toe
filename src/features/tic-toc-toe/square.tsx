e1ff2a0393c1ee845b0701f5e84a008e59356df9
rahadkc <rahadshz@gmail.com>
2021-10-07 15:24:45 +0600

    Square: update change square if game is draw

--- src/features/tic-toc-toe/square.tsx
+++ src/features/tic-toc-toe/square.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useAppSelector } from '../../app/hooks'
+import { gameStatus, selectStatus } from './gameSlice'
 
 export type ISquareProps = {
   value: string | null | JSX.Element
@@ -8,8 +10,10 @@ export type ISquareProps = {
 }
 
 const Square: React.FC<ISquareProps> = ({ value, onClick, isInWinner }) => {
+  const { draw } = useAppSelector(selectStatus)
+
   return (
-    <Button isInWinner={isInWinner} onClick={onClick}>
+    <Button draw={draw} isInWinner={isInWinner} onClick={onClick}>
       {value}
     </Button>
   )
@@ -17,14 +21,21 @@ const Square: React.FC<ISquareProps> = ({ value, onClick, isInWinner }) => {
 
 interface Props {
   isInWinner: boolean | undefined
+  draw?: boolean
 }
 
 const Button = styled.button<Props>`
   box-shadow: none;
   border-width: 2px;
   border-style: solid;
-  border-color: ${p => (p.isInWinner ? 'var(--success-border-color)' : 'var(--border-color)')};
-  background: ${p => (p.isInWinner ? 'var(--success-color)' : 'var(--secondary-color)')};
+  border-color: ${p =>
+    p.isInWinner
+      ? 'var(--success-border-color)'
+      : p.draw
+      ? 'var(--warning)'
+      : 'var(--border-color)'};
+  background: ${p =>
+    p.isInWinner ? 'var(--success-color)' : p.draw ? 'var(--warning)' : 'var(--secondary-color)'};
   height: var(--cell-size);
   box-shadow: -4px -3px 45px 21px rgb(1 1 1 / 35%);
 `
