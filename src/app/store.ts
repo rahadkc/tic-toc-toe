b250e63b6db53288493852bbe7212ed2f92e473a
rahadkc <rahadshz@gmail.com>
2021-10-07 19:39:14 +0600

    Config redux-persist to persist REDUX data

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,41 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import { combineReducers } from 'redux'
+import storage from 'redux-persist/lib/storage'
 import gameReducer from '../features/tic-toc-toe/gameSlice'
 
-export const store = configureStore({
-  reducer: {
-    game: gameReducer
-  }
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage
+}
+
+const reducers = combineReducers({
+  game: gameReducer
 })
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+})
+
+export let persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
