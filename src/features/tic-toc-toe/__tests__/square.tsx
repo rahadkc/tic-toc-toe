fc0073156a79a1baa5a0a8507866f5d60e73a4c0
rahadkc <rahadshz@gmail.com>
2021-10-07 16:05:46 +0600

    Square component cosume redux data so needed to wrap with redux Provider

--- src/features/tic-toc-toe/__tests__/square.tsx
+++ src/features/tic-toc-toe/__tests__/square.tsx
@@ -1,19 +1,29 @@
 import { cleanup, render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import { store } from '../../../app/store'
 import Square from '../square'
 
 describe('<Square>', () => {
   afterEach(cleanup)
 
-  test('render Square component with null value', () => {
+  test('square: render Square component with null value', () => {
     const value = null
-    const element = render(<Square value={value} onClick={() => {}} />)
+    const element = render(
+      <Provider store={store}>
+        <Square value={value} onClick={() => {}} />
+      </Provider>
+    )
     expect(element.container.firstChild).toBeEmpty()
     expect(element).toMatchSnapshot()
   })
 
-  test('render Square component with string value', () => {
+  test('square: render Square component with string value', () => {
     const value = 'test'
-    const element = render(<Square value={value} onClick={() => {}} />)
+    const element = render(
+      <Provider store={store}>
+        <Square value={value} onClick={() => {}} />
+      </Provider>
+    )
     expect(element.container.firstChild).not.toBeEmpty()
     expect(element.queryByText(value)).toBeInTheDocument()
     expect(element).toMatchSnapshot()
